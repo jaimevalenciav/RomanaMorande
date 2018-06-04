ec77a78f30643ab759cda424ff6ad8084e9903ad
jaimevalenciav <38919168+jaimevalenciav@users.noreply.github.com>
2018-06-04 18:09:56 -0400

    Cambios en Reporte
    Se agrega parámetro de impresion de usuario quien imprime

--- RomanaMorande/Reportes/frmreportecomprobante.vb
+++ RomanaMorande/Reportes/frmreportecomprobante.vb
@@ -3,8 +3,8 @@
 Public Class frmreportecomprobante
     Private Sub frmreportecomprobante_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        'Dim usuario As New ReportParameter("usuario", frmPrincipal.lbluser.Text)
-        'ReportViewer1.LocalReport.SetParameters(usuario)
+        Dim usuario As New ReportParameter("usuario", frmPrincipal.lbluser.Text)
+        ReportViewer1.LocalReport.SetParameters(usuario)
 
         'TODO: esta línea de código carga datos en la tabla 'pesajeDataSet2.reporte_pesaje' Puede moverla o quitarla según sea necesario.
         Me.reporte_pesajeTableAdapter.Fill(Me.pesajeDataSet8.reporte_pesaje, idpesaje:=txtidpesaje.Text)
